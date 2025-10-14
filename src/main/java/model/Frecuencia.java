64b4b536a0f1cb416db646d3ad2599fed9757528
Natalia Morales <natii.vmorales98@gmail.com>
2025-10-13 22:57:33 -0300

    se agregaron clases

---
+++ src/main/java/model/Frecuencia.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Frecuencia {
+    protected Double valor;
+
+    public Frecuencia(Double valor) {
+        this.valor = valor;
+    }
+
+    public Double getValor() {return valor;}
+
+    public void setValor(Double valor) {this.valor = valor;}
+}
---
+++ src/main/java/model/FrecuenciaCardiaca.java
@@ -0,0 +1,7 @@
+package model;
+
+public class FrecuenciaCardiaca extends Frecuencia {
+    public FrecuenciaCardiaca(Double valor) {
+        super(valor);
+    }
+}
